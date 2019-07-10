4e21aaa930cdd9f3b27c03d574c29c25280e96d9
Mc Glenn Tangalin <35588091+mcglenn07@users.noreply.github.com>
2019-07-10 10:05:22 +0800

    minor changes

--- handlers/data.php
+++ handlers/data.php
@@ -1,14 +1,75 @@
 <?php
+
 $_POST = json_decode(file_get_contents('php://input'), true);
 
 require_once '../db.php';
 
-$con = new pdo_db("temperature");
+$con = new pdo_db();
 
-$entries_list = $con->getData("SELECT temperature.id temperature, temperature.location_id, temperature.sensor_value value, FROM temperature ORDER BY temperature.system_log ASC");
+$id = $_POST['id'];
 
+$first = date("Y-m-d",strtotime("-7 Day",strtotime(date("Y-m-d"))));
+$now = date("Y-m-d",strtotime("-1 Day",strtotime(date("Y-m-d"))));
 
-echo json_encode($entries_list);
+$charts = array(
+	"temperature"=>[],
+	"humidity"=>[],
+	"soil"=>[],
+	"dew"=>[],
+	"rain"=>[],
+);
+
+$gauges = [];
+
+for ( $day=$first; strtotime($day)<=strtotime($now); $day=date("Y-m-d",strtotime("+1 Day",strtotime(date($day)))) ) {
+
+	// temperature | temperatures
+	$charts['temperature']['dates'][] = date("n/j/Y (D)",strtotime($day));
+	$q_temperature = $con->getData("SELECT IFNULL(AVG(sensor_value),0) sensor_value FROM temperatures WHERE location_id = $id AND system_log LIKE '$day%'");
+	$temperature = 0;
+	if (count($q_temperature)) {
+		$temperature = $q_temperature[0]['sensor_value'];	
+	};
+	$charts['temperature']['data'][] = abs(ceil($temperature));
+	
+	// humidity | humidities
+	$q_humidity = $con->getData("SELECT IFNULL(AVG(sensor_value),0) sensor_value FROM humidities WHERE location_id = $id AND system_log LIKE '$day%'");	
+	$charts['humidity']['dates'][] = date("n/j/Y (D)",strtotime($day));	
+	$humidity = 0;
+	if (count($q_humidity)) {
+		$humidity = $q_humidity[0]['sensor_value'];
+	};
+	$charts['humidity']['data'][] = abs(ceil($humidity));	
+	
+	// soil | soils
+	$q_soil = $con->getData("SELECT IFNULL(AVG(sensor_value),0) sensor_value FROM soils WHERE location_id = $id AND system_log LIKE '$day%'");
+	$charts['soil']['dates'][] = date("n/j/Y (D)",strtotime($day));	
+	$soil = 0;
+	if (count($q_soil)) {
+		$soil = $q_soil[0]['sensor_value'];		
+	};
+	$charts['soil']['data'][] = abs(ceil($soil));		
+	
+	// moisture dew | moisture_dews
+	$q_moisture_dew = $con->getData("SELECT IFNULL(AVG(sensor_value),0) sensor_value FROM moisture_dews WHERE location_id = $id AND system_log LIKE '$day%'");	
+	$charts['dew']['dates'][] = date("n/j/Y (D)",strtotime($day));	
+	$moisture_dew = 0;
+	if (count($q_moisture_dew)) {
+		$moisture_dew = $q_moisture_dew[0]['sensor_value'];		
+	};
+	$charts['dew']['data'][] = abs(ceil($moisture_dew));	
+	
+	// moisture rain | moisture_rains
+	$q_moisture_rain = $con->getData("SELECT IFNULL(AVG(sensor_value),0) sensor_value FROM moisture_rains WHERE location_id = $id AND system_log LIKE '$day%'");	
+	$charts['rain']['dates'][] = date("n/j/Y (D)",strtotime($day));	
+	$moisture_rain = 0;
+	if (count($q_moisture_rain)) {
+		$moisture_rain = $q_moisture_rain[0]['sensor_value'];		
+	};
+	$charts['rain']['data'][] = abs(ceil($moisture_rain));		
+	
+};
+
+echo json_encode(array("charts"=>$charts,"gauges"=>$gauges));
 
 ?>
-
